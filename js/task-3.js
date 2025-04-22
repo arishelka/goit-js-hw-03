775d6be2813e6e1ffa3a13bec803672b9f145902
arishelka <arishelka181@gmail.com>
2025-04-22 14:27:36 +0200

    added

---
+++ js/task-3.js
@@ -0,0 +1,11 @@
+function filterArray(numbers, value) {
+  const filteredNumbers = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > value) {
+      filteredNumbers.push(numbers[i]);
+    }
+  }
+
+  return filteredNumbers;
+}
